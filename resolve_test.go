9f0106a7a767b99da17f9e6662ff1a262d349c6a
Ivan Stasiuk <brokeyourbike@gmail.com>
2023-12-01 07:08:26 +0000

    test: more assertions

--- resolve_test.go
+++ resolve_test.go
@@ -9,6 +9,8 @@ import (
 	"testing"
 
 	"github.com/brokeyourbike/paystack-api-client-go"
+	"github.com/sirupsen/logrus"
+	logrustest "github.com/sirupsen/logrus/hooks/test"
 	"github.com/stretchr/testify/assert"
 	mock "github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
@@ -22,15 +24,20 @@ var resolveFailAuth []byte
 
 func TestResolveSuccess(t *testing.T) {
 	mockHttpClient := paystack.NewMockHttpClient(t)
-	client := paystack.NewClient("token", paystack.WithHTTPClient(mockHttpClient))
+
+	logger, hook := logrustest.NewNullLogger()
+	logger.SetLevel(logrus.DebugLevel)
+
+	client := paystack.NewClient("token", paystack.WithHTTPClient(mockHttpClient), paystack.WithBaseURL("https://a.com"), paystack.WithLogger(logger))
 
 	resp := &http.Response{StatusCode: 200, Body: io.NopCloser(bytes.NewReader(resolveSuccess))}
-	mockHttpClient.On("Do", mock.AnythingOfType("*http.Request")).Return(resp, nil).Once()
+	mockHttpClient.On("Do", mock.MatchedBy(func(req *http.Request) bool { return req.Host == "a.com" })).Return(resp, nil).Once()
 
 	got, err := client.ResolveBankAccount(context.TODO(), "123", "123456789")
 	require.NoError(t, err)
 
 	assert.True(t, got.Status)
+	assert.Equal(t, 2, len(hook.Entries))
 }
 
 func TestResolveFailedAuth(t *testing.T) {
@@ -38,7 +45,7 @@ func TestResolveFailedAuth(t *testing.T) {
 	client := paystack.NewClient("token", paystack.WithHTTPClient(mockHttpClient))
 
 	resp := &http.Response{StatusCode: 401, Body: io.NopCloser(bytes.NewReader(resolveFailAuth))}
-	mockHttpClient.On("Do", mock.AnythingOfType("*http.Request")).Return(resp, nil).Once()
+	mockHttpClient.On("Do", mock.MatchedBy(func(req *http.Request) bool { return req.Host == "api.paystack.co" })).Return(resp, nil).Once()
 
 	got, err := client.ResolveBankAccount(context.TODO(), "123", "123456789")
 	require.NoError(t, err)
